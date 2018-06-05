341bda4bc55e4ea2301039811c3b5b63a1a3e32e
laekov <laekov.h@gmail.com>
2018-06-05 10:50:17 -0400

    final scene

--- data/final.py
+++ data/final.py
@@ -6,29 +6,58 @@ def makescene():
     # Floor
     facestrs.append(face2str((-300, 0, 0), (300, 0, 0),
                              (-300, 300, 0), (300, 300, 0),
-                             (90, ) * 3, smooth=0.00001))
+                             (90, ) * 3, smooth=0.01))
+    # Wall
+    facestrs.append(face2str((-50, 0, 0), (-50, 300, 0),
+                             (-50, 0, 100), (-50, 300, 100),
+                             (255, 200, 200), smooth=0))
+    facestrs.append(face2str((150, 0, 0), (150, 300, 0),
+                             (150, 0, 100), (150, 300, 100),
+                             (200, 255, 200), smooth=0))
+    # Back
+    facestrs.append(face2str((-50, 300, 0), (150, 300, 0),
+                             (-50, 300, 100), (150, 300, 100),
+                             (200, 200, 255), smooth=0))
     # Top light
-    facestrs.append(face2str((-20, 0, 100), (-20, 200, 100),
-                             (120, 0, 120), (0, 200, 120),
-                             (512, ) * 3))
-    facestrs.append(face2str((150, 0, 140), (150, 200, 140),
-                             (180, 0, 120), (180, 200, 120),
+    facestrs.append(face2str((-20, 50, 130), (-20, 150, 130),
+                             (180, 50, 120), (180, 150, 120),
                              (512, ) * 3))
     curves = [
-        [(0, 0), (0, 10), (3, 3)],
-        [(3, 3), (6, 2), (9, 4)],
-        [(9, 4), (10, 6), (11, 4)],
-        [(11, 4), (15, 8), (20, 0)]
+        [(0, 0), (0, 5), (3, 1.8)],
+        [(3, 1.8), (6, 1.2), (7, 1.8)],
+        [(7, 1.8), (8, 2.4), (9, 1.8)],
+        [(9, 1.8), (10, 2.4), (12, 2.4)],
+        [(12, 2.4), (12, 2.0), (11, 0)]
+    ]
+
+    l = 0.
+    for curve in curves:
+        print_curve(curve)
+        dl = curve[2][0] - l
+        x = l * 12 / 13 + 3
+        z = l * 5 / 13 + 3
+        dx = dl * 12 / 13
+        dz = dl * 5 / 13
+        facestrs.append(bezierCurve((x * 3, 90, z * 3), (dx * 3, 0, dz * 3),
+                                    curve,
+                                    (225, 225, 255), smooth=0.5, alpha=0.7))
+        l += dl
+   
+    curves = [
+        [(0, 0), (0, 7), (3, 1.8)],
+        [(3, 1.8), (6, 1.2), (9, 1.8)],
+        [(9, 1.8), (10, 2.4), (11, 1.8)],
+        [(11, 1.8), (15, 3.6), (20, 0)]
     ]
     z = 0
     for curve in curves:
-        print_curve(curve)
+        # print_curve(curve)
         dz = curve[2][0] - z
-        facestrs.append(bezierCurve((10, 90, z * 4), (0, 0, dz * 4),
+        facestrs.append(bezierCurve((10, 120, z * 3), (0, 0, dz * 3),
                                     curve,
                                     (55, ) * 3, smooth=0.5))
         z += dz
-    facestrs.append(ball((10, 90, z + 10), 10, (55, ) * 3, smooth=0.5))
+    facestrs.append(ball((10, 120, z * 3 + 3), 3, (55, ) * 3, smooth=0.5))
     return facestrs
     # Back light
     back_light = False
